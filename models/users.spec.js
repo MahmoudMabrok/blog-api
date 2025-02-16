7665a3042eb38e809be7e3b262c66c36a4caf130
mahmoud.maghrabia@vodafone.com <mahmoud.maghrabia@vodafone.com>
2025-02-16 15:30:04 +0200

    test: validate method & use commonJs

--- models/users.spec.js
+++ models/users.spec.js
@@ -1,4 +1,4 @@
-
+const users = require("./users");
 
 describe('Users', () => {
   
@@ -6,13 +6,105 @@ describe('Users', () => {
 
   });
 
-
- describe('validateUser', () => {
-    it('should return a user by id', async () => {
-      const user = await User.findById('5f3f5f3f5f3f5f3f5f3f5f3f');
-      expect(user).toHaveProperty('name', 'John Doe');
+  describe('validateUser', () => {
+    it('should fail when name is not provided', () => {
+      const result = users.validateUser({ email: 'test@example.com', password: 'Password123' });
+      expect(result).toEqual(
+        expect.objectContaining({
+          error: expect.objectContaining({
+            message: expect.stringContaining('"name" is required'),
+          })
+        })
+      );
     });
-  });
+
+    it('should fail when name is not valid schedma', () => {
+      const result = users.validateUser({ name: "a",email: 'test@example.com', password: 'Password123' });
+      expect(result).toEqual(
+        expect.objectContaining({
+          error: expect.objectContaining({
+            message: expect.stringContaining('length must be at least 3 characters long'),
+          })
+        })
+      );
+    });
+
+    it('should fail when email is not provided', () => {
+      const result = users.validateUser({ name: 'John Doe', password: 'Password123' });
+      expect(result).toEqual(
+        expect.objectContaining({
+          error: expect.objectContaining({
+            message: expect.stringContaining('"email" is required'),
+          })
+        })
+      );
+    });
+
+    it('should fail when email is not valid', () => {
+      const result = users.validateUser({ name: 'John Doe', email: 'testMailMail' , password: 'Password123' });
+      expect(result).toEqual(
+        expect.objectContaining({
+          error: expect.objectContaining({
+            message: expect.stringContaining('must be a valid email'),
+          })
+        })
+      );
+    });
+
+    it('should fail when password is not provided', () => {
+      const result = users.validateUser({ name: 'John Doe', email: 'sasas@sasasa.com',});
+
+      expect(result).toEqual(
+        expect.objectContaining({
+          error: expect.objectContaining({
+            message: expect.stringContaining('"password" is required'),
+          })
+        })
+      );
+    });
+
+
+    it('should fail when password is not valid format', () => {
+      const result = users.validateUser({ name: 'John Doe', email: 'sasas@sasasa.com', password: '123'});
+
+      expect(result).toEqual(
+        expect.objectContaining({
+          error: expect.objectContaining({
+            message: expect.stringContaining('length must be at least 8 characters long'),
+          })
+        })
+      );
+    });
+
+    it('should fail when password is not valid format with pattern number only', () => {
+      const result = users.validateUser({ name: 'John Doe', email: 'sasas@sasasa.com', password: '123467897'});
+
+      expect(result).toEqual(
+        expect.objectContaining({
+          error: expect.objectContaining({
+            message: expect.stringContaining('fails to match the required pattern'),
+          })
+        })
+      );
+    });
+
+    it('should fail when password is not valid format with pattern character only', () => {
+      const result = users.validateUser({ name: 'John Doe', email: 'sasas@sasasa.com', password: 'asdfghjkl'});
+
+      expect(result).toEqual(
+        expect.objectContaining({
+          error: expect.objectContaining({
+            message: expect.stringContaining('fails to match the required pattern'),
+          })
+        })
+      );
+    });
+
+    it('should pass with proper data', () => {
+      const result = users.validateUser({ name: 'John Doe', email: 'email@example.com', password: 'a12234556678'});
+
+      expect(result.error).toBeNull();
+    });
+});
 
 });
-    
