b798388713763f85da3d857a58e7a489304bdc0e
mahmoudmabrok <mahmoudmabrok3579@gmail.com>
2025-02-24 21:44:24 +0200

    add: security parts

--- models/users/users.js
+++ models/users/users.js
@@ -7,7 +7,7 @@ const userSchema = new Schema({
   name: {
     type: String,
     required: true,
-    minlength: 5,
+    minlength: 3,
     maxlength: 50,
   },
   email: {
--- startup/security.js
+++ startup/security.js
@@ -1,5 +1,9 @@
 const helmet = require("helmet");
+var cors = require('cors');
+const { xss } = require('express-xss-sanitizer');
 
 module.exports = function(app) {
     app.use(helmet());
+    app.use(cors());
+    app.use(xss());
 }
